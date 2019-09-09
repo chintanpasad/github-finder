ed2239ad20bf5b5966a275f29b8cbbdcdb587d3e
Chintan Pasad <chintanpasad1701@gmail.com>
2019-09-09 20:18:53 +0530

    Add Users and UserItem components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 function App() {
 	return (
 		<Fragment>
 			<Navbar />
 			<div className='container'>
-				<UserItem />
+				<Users />
 			</div>
 		</Fragment>
 	);
