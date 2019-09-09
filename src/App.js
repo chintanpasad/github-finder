4789a7b7d562d2ca3d7e10be8e6af530b3c6255b
Chintan Pasad <chintanpasad1701@gmail.com>
2019-09-09 20:07:56 +0530

    Add UserItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
-import Navbar from './components/layout/Navbar';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 
 function App() {
 	return (
 		<Fragment>
 			<Navbar />
+			<div className='container'>
+				<UserItem />
+			</div>
 		</Fragment>
 	);
 }
