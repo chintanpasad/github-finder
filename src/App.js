d151e28ba1307ba08e686a5ccee7c5dbec0e53be
Chintan Pasad <chintanpasad1701@gmail.com>
2019-09-10 09:30:56 +0530

    Implement Search functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
+import Search from './components/users/Search';
 
-function App() {
-	return (
-		<Fragment>
-			<Navbar />
-			<div className='container'>
-				<Users />
-			</div>
-		</Fragment>
-	);
+class App extends Component {
+	state = {
+		users: [],
+		loading: false
+	};
+
+	// async componentDidMount() {
+	// 	this.setState({ loading: true });
+	// 	const res = await axios.get(
+	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+	// 	);
+	// 	this.setState({ users: res.data, loading: false });
+	// }
+
+	searchUsers = async text => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ users: res.data.items, loading: false });
+	};
+
+	clearUsers = () => this.setState({ users: [] });
+
+	render() {
+		return (
+			<Fragment>
+				<Navbar />
+				<div className='container'>
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={this.state.users.length > 0 ? true : false}
+					/>
+					<Users loading={this.state.loading} users={this.state.users} />
+				</div>
+			</Fragment>
+		);
+	}
 }
 
 export default App;
