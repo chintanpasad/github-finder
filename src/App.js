116e15c256d2077adc8a101de536f3ed2dd44593
Chintan Pasad <chintanpasad1701@gmail.com>
2019-09-10 12:03:08 +0530

    Add single user component with data & UI

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import User from './components/users/User';
 
 class App extends Component {
 	state = {
 		users: [],
+		user: {},
 		loading: false,
-		alert: null
+		alert: null,
+		repos: []
 	};
 
 	// async componentDidMount() {
@@ -36,6 +39,22 @@ class App extends Component {
 	showAlert = (msg, type) => {
 		this.setState({ alert: { msg, type } });
 		setTimeout(() => this.setState({ alert: null }), 5000);
+	};
+
+	getUser = async username => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ user: res.data, loading: false });
+	};
+
+	getUserRepos = async username => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ repos: res.data, loading: false });
 	};
 
 	render() {
@@ -65,6 +84,20 @@ class App extends Component {
 								)}
 							/>
 							<Route exact path='/about' component={About} />
+							<Route
+								exact
+								path='/user/:login'
+								render={props => (
+									<User
+										{...props}
+										getUser={this.getUser}
+										getUserRepos={this.getUserRepos}
+										user={this.state.user}
+										repos={this.state.repos}
+										loading={this.state.loading}
+									/>
+								)}
+							/>
 						</Switch>
 					</div>
 				</Fragment>
