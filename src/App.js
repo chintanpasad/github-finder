298ba29f2950268781db8d40a6b282616037d5b0
Chintan Pasad <chintanpasad1701@gmail.com>
2019-09-10 19:03:12 +0530

    Alert Context workflow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
@@ -8,39 +8,35 @@ import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
-	const [alert, setAlert] = useState(null);
-
-	const showAlert = (msg, type) => {
-		setAlert({ msg, type });
-		setTimeout(() => setAlert(null), 5000);
-	};
-
 	return (
 		<GithubState>
-			<Router>
-				<Fragment>
-					<Navbar />
-					<div className='container'>
-						<Alert alert={alert} />
-						<Switch>
-							<Route
-								exact
-								path='/'
-								render={props => (
-									<Fragment>
-										<Search showAlert={showAlert} />
-										<Users />
-									</Fragment>
-								)}
-							/>
-							<Route exact path='/about' component={About} />
-							<Route exact path='/user/:login' component={User} />
-						</Switch>
-					</div>
-				</Fragment>
-			</Router>
+			<AlertState>
+				<Router>
+					<Fragment>
+						<Navbar />
+						<div className='container'>
+							<Alert />
+							<Switch>
+								<Route
+									exact
+									path='/'
+									render={props => (
+										<Fragment>
+											<Search />
+											<Users />
+										</Fragment>
+									)}
+								/>
+								<Route exact path='/about' component={About} />
+								<Route exact path='/user/:login' component={User} />
+							</Switch>
+						</div>
+					</Fragment>
+				</Router>
+			</AlertState>
 		</GithubState>
 	);
 };
